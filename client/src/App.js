7d1cf3b1878e5f317c88b031c69ab783bc6bc61d
React-Management-Tutorial <heyback@naver.com>
2022-03-24 11:28:01 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -22,34 +22,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-    {
-    'id':1,
-    'image':'https://placeimg.com/64/64/1',
-    'name':'홍길동',
-    'birthday':'19001212',
-    'gender':'남자',
-    'job':'직장인'
-  },
-  {
-    'id':2,
-    'image':'https://placeimg.com/64/64/2',
-    'name':'나문희',
-    'birthday':'19101212',
-    'gender':'여자',
-    'job':'가정주부'
-  },
-  {
-    'id':3,
-    'image':'https://placeimg.com/64/64/3',
-    'name':'나요',
-    'birthday':'19201212',
-    'gender':'남자',
-    'job':'무직'
-  }
-]
+
 
 class App extends Component{
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render(){
     const { classes } = this.props;
     return (
@@ -64,7 +54,9 @@ class App extends Component{
             <TableCell>직업</TableCell>
           </TableHead>
           <TableBody>
-          {customers.map(c => {return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+          {this.state.customers ? this.state.customers.map(c => {
+            return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthdayW} gender={c.gender} job={c.job} />)
+          }) : ""}
           </TableBody>
         </table>
 
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello',(req,res) => {
-    res.send({message:'Hello Express!'});
+app.get('/api/customers',(req,res) => {
+    res.send([
+        {
+            'id':1,
+            'image':'https://placeimg.com/64/64/1',
+            'name':'홍길동',
+            'birthday':'19001212',
+            'gender':'남자',
+            'job':'직장인'
+          },
+          {
+            'id':2,
+            'image':'https://placeimg.com/64/64/2',
+            'name':'나문희',
+            'birthday':'19101212',
+            'gender':'여자',
+            'job':'가정주부'
+          },
+          {
+            'id':3,
+            'image':'https://placeimg.com/64/64/3',
+            'name':'나요',
+            'birthday':'19201212',
+            'gender':'남자',
+            'job':'무직'
+          }
+    ])
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
